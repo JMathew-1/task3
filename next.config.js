e2c338d6d4bc471e612c35ebfaea952641930618
JMathew-1 <151427103+JMathew-1@users.noreply.github.com>
2024-07-24 17:04:53 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   pageExtensions: ['jsx', 'js'], // include .jsx and .js files as Next.js pages
-  // You can add other Next.js configurations here
+  basePath: '/your-repo-name',
 };
